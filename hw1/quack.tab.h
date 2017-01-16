8d128e4cf12a553fe69e791969d30e403167ad5f
awh <andrew.woroby.hampton@gmail.com>
2017-01-16 14:27:38 -0800

    started working on this!

---
+++ hw1/quack.tab.h
@@ -0,0 +1,29 @@
+#ifndef YYTOKENTYPE
+# define YYTOKENTYPE
+  enum yytokentype
+  {
+    CLASS = 258,
+    DEF = 259,
+    EXTENDS = 260,
+    IF = 261,
+    ELIF = 262,
+    ELSE = 263,
+    WHILE = 264,
+    RETURN = 265,
+    ATLEAST = 266,
+    ATMOST = 267,
+    EQUALS = 268,
+    AND = 269,
+    OR = 270,
+    NOT = 271,
+    IDENT = 272,
+    INT_LIT = 273,
+    STRING_LIT = 274,
+    LESS = 275,
+    MORE = 276,
+    PLUS = 277,
+    MINUS = 278,
+    TIMES = 279,
+    DIVIDE = 280
+  };
+#endif
